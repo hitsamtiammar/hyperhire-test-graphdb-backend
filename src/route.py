e2f3939a91e79ab22a335dcf661df28c701d1d8d
Hitsam Tiammar <hitsam@tetherfi.com>
2024-08-13 22:08:38 +0700

    testing routing

--- src/route.py
+++ src/route.py
@@ -1,5 +1,11 @@
 from server import app
+from flask import request
 
 @app.route('/')
 def index():
     return {"Data": "12345"}
+
+@app.route('/post', methods=['POST'])
+def post():
+    print('Hello 12345 44444')
+    return {'Data': 'This is post example', 'd': request.json['Hello'] + 'Lalalal'}
