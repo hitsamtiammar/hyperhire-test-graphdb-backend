cc34af025fc329af8f988bf3de09203f113099bc
Hitsam Tiammar <lossantos3000@gmail.com>
2024-08-14 23:31:40 +0700

    implement multiple server running

--- src/route.py
+++ src/route.py
@@ -10,43 +10,50 @@ def index():
 
 @app.route('/create-database', methods=['POST'])
 def create_database():
-    print('Hello 12345 44444')
+    working_directory = os.getcwd()
+    os.chdir(working_directory)
     parent_dir = os.path.dirname(os.path.realpath(__file__))
-    dir = "blazegraph-servers/9000"
-    #dir = os.path.join(os.getcwd(), dir) 
-    print('hehe: ' +os.path.join(parent_dir, dir))
-    dir = os.path.join(parent_dir, dir)
-    if not os.path.exists(dir):
-        os.mkdir(dir)
 
-    process_result = subprocess.Popen(['python', parent_dir + '/../test.py' ])
-    print('this is dir ' + dir)
-    print('This is output')
-    print(process_result.stdout)
+    blazegraph_dir = os.path.join(parent_dir, 'blazegraph-servers')
 
-    # target_dir = os.path.join(parent_dir, 'blazegraph-servers', '8787')
-    
-    # if not os.path.exists(target_dir):
-    #     os.mkdir(target_dir)
-    #     shutil.copy(os.path.join(parent_dir, 'blazegraph.jar'), os.path.join(target_dir, 'blazegraph.jar') )
+    if not os.path.exists(blazegraph_dir):
+        os.mkdir(blazegraph_dir)
+
+    print('blazegraph_dir: ' + blazegraph_dir)
+    print('working_directory: ' + parent_dir)
+    port = request.json['port']
+    target_dir = os.path.join(blazegraph_dir, str(port))
+
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    blazegraph_target_dir =os.path.join(target_dir, 'blazegraph.jar')
+
+    if not os.path.exists(blazegraph_target_dir):
+        shutil.copy(os.path.join(parent_dir, 'blazegraph.jar'), blazegraph_target_dir)
+
+    maximum_usage = request.json['minimumUsage'] if 'minimumUsage' in request.json else None 
+    minimum_usage = request.json['minimumUsage'] if 'minimumUsage' in request.json else None 
+
+    maximum_usage_cmd = f"-Xmx{maximum_usage}g" if maximum_usage is not None else ''
+    minimum_usage_cmd = f"-Xms{minimum_usage}g" if minimum_usage is not None else ''
 
 
-    str_command = 'java -server -Xmx10g -Djetty.port=<port> -jar <blazegraph>'
+    str_command = f"java -server {maximum_usage_cmd} {minimum_usage_cmd} -Djetty.port={port} -jar <blazegraph>"
+    str_command = str_command.replace('<blazegraph>', os.path.join(target_dir, 'blazegraph.jar'))
 
-    str_command = str_command.replace('<port>', '8787')\
-        .replace('<blazegraph>', os.path.join(parent_dir, 'blazegraph.jar'))
+    os.chdir(os.path.join(target_dir))
 
-    print(str_command)
+    log_file = os.path.join(target_dir, 'file.log')
+    flag = 'w'
 
-    print(str_command.split(' '))
+    if os.path.exists(log_file):
+        flag = 'a'
 
-    process_result = subprocess.Popen(['python', parent_dir + '/../test.py' ])
+    out_file = open(log_file, flag)
 
-    process_result_blaze = subprocess.Popen(str_command.split(' '))
+    subprocess.Popen(str_command.split(' '), stdout=out_file)
 
-    print('pid:')
-    #print(process_result_blaze.pid)
-  
-    # print(output) 
-    # print(error) 
-    return {'Data': 'This is post example', 'all': request.json}
+    return {
+        'status': 'Success',
+        'message': 'Wait for a while before the server is ready to connect'
+    }
